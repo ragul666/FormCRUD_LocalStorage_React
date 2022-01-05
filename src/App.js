57a376d5ce2195cddfc16d5c1cc32471a98624e6
ragul666 <94818396+ragulkv1998@users.noreply.github.com>
2022-01-05 19:26:23 +0530

    view added

--- src/App.js
+++ src/App.js
@@ -1,108 +1,129 @@
-import React,{useState, useEffect} from 'react'
-import { View } from './components/View';
+import React, { useState, useEffect } from "react";
+import { View } from "./components/View";
 
 // getting the values of local storage
-const getDatafromLS=()=>{
-  const data = localStorage.getItem('books');
-  if(data){
+const getDatafromLS = () => {
+  const data = localStorage.getItem("books");
+  if (data) {
     return JSON.parse(data);
+  } else {
+    return [];
   }
-  else{
-    return []
-  }
-}
+};
 
 export const App = () => {
-
   // main array of objects state || books state || books array of objects
-  const [books, setbooks]=useState(getDatafromLS());
+  const [books, setbooks] = useState(getDatafromLS());
 
   // input field states
-  const [title, setTitle]=useState('');
-  const [author, setAuthor]=useState('');
-  const [isbn, setIsbn]=useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState("");
+  const [isbn, setIsbn] = useState("");
 
   // form submit event
-  const handleAddBookSubmit=(e)=>{
+  const handleAddBookSubmit = (e) => {
     e.preventDefault();
     // creating an object
-    let book={
+    let book = {
       title,
-      author,
-      isbn
-    }
-    setbooks([...books,book]);
-    setTitle('');
-    setAuthor('');
-    setIsbn('');
-  }
+      description,
+      isbn,
+      rating,
+    };
+    setbooks([...books, book]);
+    setTitle("");
+    setDescription("");
+    setIsbn("");
+    setRating("");
+  };
 
   // delete book from LS
-  const deleteBook=(isbn)=>{
-    const filteredBooks=books.filter((element,index)=>{
-      return element.isbn !== isbn
-    })
+  const deleteBook = (isbn) => {
+    const filteredBooks = books.filter((element, index) => {
+      return element.isbn !== isbn;
+    });
     setbooks(filteredBooks);
-  }
+  };
 
   // saving data to local storage
-  useEffect(()=>{
-    localStorage.setItem('books',JSON.stringify(books));
-  },[books])
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
-    <div className='wrapper'>
-      <h1>BookList App</h1>
-      <p>Add and view your books using local storage</p>
-      <div className='main'>
+    <div className="wrapper">
+      <h1>Form CRUD Functionality </h1>
+      <p>CRUD along with validation with Bootstrap Styling</p>
+      <div className="main">
+        <div className="form-container">
+          <form
+            autoComplete="off"
+            className="form-group"
+            onSubmit={handleAddBookSubmit}
+          >
+            <label> Product Name</label>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            ></input>
 
-        <div className='form-container'>
-          <form autoComplete="off" className='form-group'
-          onSubmit={handleAddBookSubmit}>
-            <label>Title</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setTitle(e.target.value)} value={title}></input>
-            <br></br>
-            <label>Author</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setAuthor(e.target.value)} value={author}></input>
-            <br></br>
-            <label>ISBN#</label>
-            <input type="text" className='form-control' required
-            onChange={(e)=>setIsbn(e.target.value)} value={isbn}></input>
-            <br></br>
-            <button type="submit" className='btn btn-success btn-md'>
+            <label>Product Description</label>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            ></input>
+
+            <label>Price in Rupees</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="₹ "
+              required
+              onChange={(e) => setIsbn(e.target.value)}
+              value={isbn}
+            ></input>
+
+            <label>Product Rating</label>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => setRating(e.target.value)}
+              value={rating}
+            ></input>
+            <br />
+            <button type="submit" className="btn btn-success btn-md">
               ADD
             </button>
           </form>
         </div>
 
-        <div className='view-container'>
-          {books.length>0&&<>
-            <div className='table-responsive'>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th>ISBN#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <View books={books} deleteBook={deleteBook}/>
-                </tbody>
-              </table>
-            </div>
-            <button className='btn btn-danger btn-md'
-            onClick={()=>setbooks([])}>Remove All</button>
-          </>}
-          {books.length < 1 && <div>No books are added yet</div>}
+        <div className="view-container">
+          {books.length > 0 && (
+            <>
+              <div>
+                <View books={books} deleteBook={deleteBook} />
+              </div>
+              <button
+                className="btn btn-danger btn-md"
+                onClick={() => setbooks([])}
+              >
+                Remove All Products
+              </button>
+            </>
+          )}
+          {books.length < 1 && <div>No Product Cards Added yet</div>}
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/View.js
+++ src/components/View.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import {Icon} from 'react-icons-kit'
-import {trash} from 'react-icons-kit/feather/trash'
+import React from "react";
+import { Icon } from "react-icons-kit";
+import { trash } from "react-icons-kit/feather/trash";
 
-export const View = ({books,deleteBook}) => {
-    
-    return books.map(book=>(
-        
-        <tr key={book.isbn}>
-            <td>{book.isbn}</td>
-            <td>{book.title}</td>
-            <td>{book.author}</td>
-            <td className='delete-btn' onClick={()=>deleteBook(book.isbn)}>
-                <Icon icon={trash}/>
-            </td>           
-        </tr>            
-    
-))
-}
+export const View = ({ books, deleteBook }) => {
+  return books.map((book) => (
+    <div style={{ paddingBottom: "20px" }}>
+      <div class="card" key={book.isbn}>
+        <div class="card-body" style={{ border: "10px" }}>
+          <h5
+            class="card-title"
+            style={{
+              display: "flex",
+              margin: "20px",
+            }}
+          >
+            Product Name:{book.title}
+          </h5>
+        </div>
+        <p class="card-text">Product Description:{book.description}</p>
+
+        <p class="card-text">Price in Rupees: ₹{book.isbn}</p>
+        <h5
+          class="card-title"
+          style={{
+            display: "flex",
+            margin: "20px",
+          }}
+        >
+          <p class="card-text">Rating:{book.rating}</p>
+        </h5>
+        <br />
+        <p class="card-text" style={{ margin: "auto", padding: "20px" }}>
+          <td className="delete-btn" onClick={() => deleteBook(book.isbn)}>
+            <Icon icon={trash} />
+          </td>
+        </p>
+      </div>
+    </div>
+  ));
+};
