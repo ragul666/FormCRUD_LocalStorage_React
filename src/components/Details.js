c727e45cc9ad4a22c12567f4629915d2323345e7
ragul666 <94818396+ragulkv1998@users.noreply.github.com>
2022-01-06 20:02:08 +0530

    details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,43 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Details = () => {
   let location = useLocation();
   console.log(location);
-  return <div> {location.state?.title}</div>;
+  return (
+    <div>
+      <div style={{ paddingBottom: "20px" }}>
+        <div class="card-body" style={{ border: "10px" }}>
+          <h5
+            class="card-title"
+            style={{
+              display: "flex",
+              margin: "20px",
+            }}
+          >
+            Product Name:{location.state?.title}
+          </h5>
+        </div>
+
+        <p class="card-text">
+          Product Description:{location.state?.description}
+        </p>
+
+        <p class="card-text">Price in Rupees: ₹{location.state?.price}</p>
+        <h5
+          class="card-title"
+          style={{
+            display: "flex",
+            margin: "20px",
+          }}
+        >
+          <p class="card-text">Rating:{location.state?.rating}</p>
+        </h5>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default Details;
